38dec7e85bb7005b5aeff73a077d87127ee22f24
Lee Graham <graham.la3@gmail.com>
2018-09-09 16:06:37 -0600

    Implement skeletal structure of the main index js file including creating the store, wrapping the app in provider and browser router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import { Provider } from 'redux-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { rootReducer } from './reducers/index';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
