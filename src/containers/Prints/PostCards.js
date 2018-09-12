1f78f314c0dcd961f47bcf39b371306e33f8dfe0
Lee Graham <graham.la3@gmail.com>
2018-09-12 15:05:56 -0600

    Display debunked images on debunked component route

---
+++ src/containers/Prints/PostCards.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const PostCards = () => {
+  return(
+      <div className="PostCards">
+        <h1>pic a debunk image for your card</h1>
+        <h2>enter editable text</h2>
+      </div>
+    )
+}
---
+++ src/containers/Prints/Stickers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Stickers = () => {
+  return(
+      <div className="Stickers">
+        <h1>pic a debunk image for your card</h1>
+        <h2>enter editable text</h2>
+      </div>
+    )
+}
