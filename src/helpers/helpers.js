5feb23dcc4b5e01a22ad06721e4fbe1109a70a65
Lee Graham <graham.la3@gmail.com>
2018-09-15 14:09:26 -0600

    Implement code to extract the current date from the initial fetch to display the appropriate url for the image src

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,6 +1,12 @@
 export const cleanPics = (data) => {
   const newData = data.map((pic, index) => {
-    return {url: `https://epic.gsfc.nasa.gov/archive/natural/2018/09/11/png/${pic.image}.png`, id: index, text: ''}
+    const year = pic.date.split('')
+    const month = year.splice(4)
+    const date = month.splice(3)
+    date.splice(3)
+    month.shift()
+    date.shift()
+    return {url: `https://epic.gsfc.nasa.gov/archive/natural/${year.join([])}/${month.join([])}/${date.join([])}/png/${pic.image}.png`, id: index, text: ''}
   })
   return newData
 }
