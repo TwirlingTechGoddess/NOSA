9074fa8920ba6b1be1399a9c435978b84a9dbbbc
Lee Graham <graham.la3@gmail.com>
2018-09-16 19:23:33 -0600

    Implement final stylings and bug fixes for the stickers component

--- src/containers/Prints/Stickers.js
+++ src/containers/Prints/Stickers.js
@@ -76,18 +76,20 @@ class Stickers extends Component {
     return(
       <div>
         <div className="DebunksContainer">
-          <div className="Stickers">
-            <h1>{this.state.default.title}</h1>
-            <img className='currentPrintPic' alt='' src={this.state.currentPic} />
-            <h3>{this.state.default.text}</h3>
+          <div className = "TemplateContainer">
+            <div className="Stickers">
+              <h1>{this.state.default.title}</h1>
+              <img className='currentPrintPic' alt='' src={this.state.currentPic} />
+              <h3>{this.state.default.text}</h3>
+            </div>
+            <button onClick={this.toggleDebunksDisplay}>Show Debunked Images</button>
+            <button onClick={this.changeText}>Change Text</button>
+            <button onClick={this.printOut}>PRINT</button>
           </div>
           <div className={this.state.hidden}>
             {newImages}
           </div>
         </div>
-        <button onClick={this.toggleDebunksDisplay}>Show Debunked Images</button>
-        <button onClick={this.changeText}>Change Text</button>
-        <button onClick={this.printOut}>PRINT</button>
       </div>
     )
   }
