38a6237d0733b3855d8e5a031e8184518d81e842
Lee Graham <graham.la3@gmail.com>
2018-09-17 14:35:44 -0600

    Implement tests for fetchCalls file

---
+++ src/helpers/helpers.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { cleanPics } from'./helpers.js'
+
+describe('cleanPics funtion in the helpers file', () => {
+  it(" exist ", () => {
+    expect(true).toEqual(true)
+  })
+})
