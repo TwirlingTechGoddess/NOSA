7818adce72d60f877b43551b18e8fbc4cfd6d1f5
Lee Graham <graham.la3@gmail.com>
2018-09-17 21:13:30 -0600

    Implement tests for Print component

--- src/containers/Prints/Prints.js
+++ src/containers/Prints/Prints.js
@@ -2,25 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-class Prints extends Component {
-  render() {
-    return(
-      <div className="Prints">
-        <article>
-          <NavLink to='/posters'>Posters</NavLink>
-        </article>
-        <article>
-          <NavLink to='/postcards'>Post Cards</NavLink>
-        </article>
-        <article>
-          <NavLink to='/giftcards'>Gift Cards</NavLink>
-        </article>
-        <article>
-          <NavLink to='/stickers'>Stickers</NavLink>
-        </article>
-      </div>
-    )
-  }
+export const Prints = () => {
+  return(
+    <div className="Prints">
+      <article>
+        <NavLink to='/posters'>Posters</NavLink>
+      </article>
+      <article>
+        <NavLink to='/postcards'>Post Cards</NavLink>
+      </article>
+      <article>
+        <NavLink to='/giftcards'>Gift Cards</NavLink>
+      </article>
+      <article>
+        <NavLink to='/stickers'>Stickers</NavLink>
+      </article>
+    </div>
+  )
 }
-
-export default connect(null, null)(Prints)
